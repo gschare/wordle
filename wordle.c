33f812ec9c0fd5f7d493f9394cce7e3fbf852a92
Gregory Schare <grschare1315@gmail.com>
2022-02-07 01:33:42 -0500

    add alphabet at top

--- wordle.c
+++ wordle.c
@@ -8,6 +8,20 @@
 #define WORD_LEN 5
 #define MAX_GUESSES 6
 
+enum L_STATUS {
+    CORRECT,
+    WRONG,
+    UNUSED,
+    UNGUESSED
+};
+
+struct Letter {
+    enum L_STATUS status;
+    struct List *locs;
+    int times;
+    char letter;
+};
+
 int load_words(struct List *words, char *filename) {
     int count = 0;
     initList(words);
@@ -50,9 +64,11 @@ int main(int argc, char **argv) {
     int r;
     struct Node *guess_node;
     struct Node *secret;
+    char *secret_word;
     char *color;
     int c;
     int bad_guess = 0;
+    struct Letter alphabet[26];
 
     // Start a game.
     n_guess = 1;
@@ -64,8 +80,9 @@ int main(int argc, char **argv) {
         for (int i=0; i<r; i++) {
             secret = secret->next;
         }
+        secret_word = (char *)secret->data;
     } else {
-        char *secret_word = argv[1];
+        secret_word = argv[1];
         secret = findNode(&answers, secret_word, (int(*)(const void*, const void*))&strcmp);
         if (secret == NULL) {
             printf("debug word not in word list\n");
@@ -73,11 +90,57 @@ int main(int argc, char **argv) {
         }
     }
 
+    // Set up the alphabet.
+    for (char c='a'; c<='z'; c++) {
+        struct List *locs = malloc(sizeof(struct List));
+        initList(locs);
+        struct Node *prev = locs->head;
+        int count = 0;
+        for (int i=0; i<WORD_LEN; i++) {
+            if (secret_word[i] == c) {
+                int *p = malloc(sizeof(int));
+                *p = i;
+                prev = addAfter(locs, prev, p);
+                count++;
+            }
+        }
+        alphabet[c-'a'].status = UNGUESSED;
+        alphabet[c-'a'].locs   = locs;
+        alphabet[c-'a'].times  = count;
+        alphabet[c-'a'].letter = c;
+    }
+
+    // Display the alphabet and current guess at the top.
+    for (int i=0; i<26; i++) {
+        printf("%c", toupper(alphabet[i].letter));
+    }
+    printf("\n");
     printf("(%d/%d)\n", n_guess, MAX_GUESSES);
+
+    // Outer game loop for each guess.
     while (1) {
         // Take user input.
         while (1) {
             if (n_guess != 1) {
+                // Update alphabet.
+                printf("\x1b[%dF", n_guess+1);
+                printf("\x1b[2K");
+                for (int i=0; i<26; i++) {
+                    if (alphabet[i].status == UNGUESSED) {          // Normal
+                        color = "\033[0m";
+                    } else if (alphabet[i].status == UNUSED) {      // Red
+                        color = "\033[0;31m";
+                    } else if (alphabet[i].status == CORRECT) {     // Green
+                        color = "\033[0;32m";
+                    } else if (alphabet[i].status == WRONG) {       // Yellow
+                        color = "\033[0;33m";
+                    }
+                    printf("%s%c", color, toupper(alphabet[i].letter));
+                }
+                printf("\033[0m\n");
+                printf("\x1b[%dE", n_guess-1+1);
+
+                // Update guess counter.
                 printf("\x1b[%dF", n_guess);
                 if (bad_guess) {
                     printf("\x1b[1F");
@@ -118,6 +181,7 @@ int main(int argc, char **argv) {
         }
 
         // Check if it's the secret word.
+        guess_node = findNode(&answers, guess, (int(*)(const void *, const void *))&strcmp);
         if (guess_node == secret) {
             printf("\033[1;32m%s\033[0m\n", (char *)secret->data);
             printf("You got it!\n");
@@ -136,6 +200,7 @@ int main(int argc, char **argv) {
                     // see the first 's' and stop at yellow without this check.
                     color = "\033[1;32m";
                     printf("%s%c", color, guess[i]);
+                    alphabet[guess[i]-'a'].status = CORRECT;
                     continue;
                 }
                 for (int j=0; j<WORD_LEN; j++) {
@@ -144,15 +209,18 @@ int main(int argc, char **argv) {
                         if (i == j) {
                             // Letter in correct spot: print green.
                             color = "\033[1;32m";
+                            alphabet[guess[i]-'a'].status = CORRECT;
                         } else {
                             // Letter not in correct spot: print yellow.
                             color = "\033[1;33m";
+                            alphabet[guess[i]-'a'].status = WRONG;
                         }
                         break;
                     } else {
                         // Letter i of guess not in secret word.
                         // Print white.
-                        color = "\033[0m";
+                        color = "\033[0;31m";
+                        alphabet[guess[i]-'a'].status = UNUSED;
                     }
                 }
                 printf("%s%c", color, guess[i]);
@@ -169,5 +237,9 @@ int main(int argc, char **argv) {
 
     free_words(&answers);
     free_words(&guesses);
+    for (int i=0; i<26; i++) {
+        traverseList(alphabet[i].locs, &free);
+        removeAllNodes(alphabet[i].locs);
+    }
     return 0;
 }
